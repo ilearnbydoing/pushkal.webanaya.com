3a38859c8f0ed0f2dcbacfa2afb9ab1db01cf25a
ilearnbydoing <dgweb123@gmail.com>
2019-03-01 15:27:36 +0530

    update

--- form-process.php
+++ form-process.php
@@ -1,27 +1,32 @@
 <?php
 $name = $_POST["name"];
+$email = $_POST["email"];
 $phone = $_POST["phone"];
-$age = $_POST["age"];
+$company = $_POST["company"];
 
-$EmailTo = "dinesh.hnh@gmail.com,sonpreet.priyam@gmail.com,durgesh@webanaya.com";
-$EmailFrom = "info@webanaya.com";
-$Subject = "Cutie Pie - You have received New Enquiry";
+$EmailTo = "beemed2018@gmail.com,dinesh.hnh@gmail.com,durgesh@webanaya.com";
+$EmailFrom = "info@hnhtwenty.com";
+$Subject = "Pushkal Textiles - You have received New Enquiry";
 
 // prepare email body text
 $Body .= "Name: ";
 $Body .= $name;
 $Body .= "\n";
 
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+
 $Body .= "Phone: ";
 $Body .= $phone;
 $Body .= "\n";
 
-$Body .= "Age: ";
-$Body .= $age;
+$Body .= "Company: ";
+$Body .= $company;
 $Body .= "\n";
 
 // send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$EmailTo);
+$success = mail($EmailTo, $Subject, $Body, "From:".$EmailFrom);
 
 // redirect to success page
 if ($success){
